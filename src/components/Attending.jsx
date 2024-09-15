75685c1a017480de704233965ddf84e962b845e7
michael-ono <mikeonochie@outlook.com>
2024-09-15 11:10:21 +0100

    corrections made

--- src/components/Attending.jsx
+++ src/components/Attending.jsx
@@ -10,7 +10,6 @@ const Attending = () => {
 
   return (
     <>
-    {/* SPEAKERS */}
     <div className='w-full bg-[#ecf1f7]'>
       
       <div>
@@ -26,7 +25,6 @@ const Attending = () => {
               <div className="flex flex-col text-lg gap-1">
                 <p className="text font-medium">Prof. Benjamin S. Aribisala</p>
                 <p>Department of Computer Science</p>
-                <p>Faculty of Science</p>
                 <p>Lagos State University</p>
               </div>
             </div>
@@ -50,14 +48,14 @@ const Attending = () => {
               <h2 className="text-3xl text-pink-600 font-extrabold mb-2">LOC Chair</h2>
               <div className="flex flex-col text-lg gap-1">
                 <p className="text-lg font-medium">Dr. Wilson Sakpere</p>
-                <p>HOD (Computer Science)</p>
+                <p>HOD, Computer Science</p>
               </div>
             </div>
           </div>
           <div className='flex flex-col justify-center items-center text-center gap-y-4'>
             <img src={LogoFascon} className="w-[340px] h-[130px] pb-4" alt="fascon logo" />
             <h1 className='text-3xl font-bold text-red-400 pb-5'>Abstract admission is now open!!!</h1>
-            <p className='text-2xl'>Submit an abstract by 30" August 2024 to fascon@lcu.edu.ng with an email Subject containing the author's name.</p>
+            <p className='text-2xl'>Submit an abstract by 30th August 2024 to fascon@lcu.edu.ng with an email Subject containing the author's name.</p>
           </div>
         </div>
 
@@ -76,7 +74,7 @@ const Attending = () => {
 
             {/* Track4 */}
             <div className="">
-              <h1 className="text-2xl text-blue-800 font-extrabold mb-2">Track 4: Emerging Technologies for Inclusive and ustainable Education</h1>
+              <h1 className="text-2xl text-blue-800 font-extrabold mb-2">Track 4: Emerging Technologies for Inclusive and Sustainable Education</h1>
               <p className="text text-grey-800 font-medium max-w-2xl">
                 Adaptive Learning Systems; Intelligent Tutoring; Personalised Learning Pathways; Al-enhanced Virtual Learning Environments and Remote Education; Al for Efficient Curriculum Design 
                 and Development; Al-driven Predictive Analytics; Al for Automated and Fair Evaluation; Collaborative Learning and Al; Al for Educational Game Design.
@@ -157,11 +155,9 @@ const Attending = () => {
               <p className="text-xl text-black font-semibold">+234 803 496 8640</p>
             </div>
           </div>
-          
         </div>
 
       </div>
-
     </div>
   </>
   );
